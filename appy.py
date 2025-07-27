6eb1990da559c43adc01b7bccbb3bf4f2d76dedc
Olusanya Joy Naomi <91166707+joynaomi81@users.noreply.github.com>
2025-07-27 15:16:29 +0100

    Update appy.py

--- appy.py
+++ appy.py
@@ -3,7 +3,7 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.schema import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
